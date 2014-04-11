578ba4db7b0fce7021f1ddeeb4b36ab0d0e1a3fe
tshichun <1055859772@qq.com>
2014-04-11 18:32:03 +0800

    修改常量名

--- myfirst/config.m4
+++ myfirst/config.m4
@@ -1,6 +1,6 @@
 PHP_ARG_ENABLE(myfirst,
 	[Wether to enable the "myfirst" extension],
-	[	enable-myfirst		Enable "myfirst" extension support])
+	[enable-myfirst	Enable "myfirst" extension support])
 
 if test $PHP_MYFIRST != "no"; then
 	PHP_SUBST(MYFIRST_SHARED_LIBADD)
--- myfirst/myfirst.h
+++ myfirst/myfirst.h
@@ -1,11 +1,11 @@
-#ifndef PHP_MYFIRST_H
-#define PHP_MYFIRST_H
+#ifndef MYFIRST_H
+#define MYFIRST_H
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 #include "php.h"
-#define phpext_myfirst_ptr &myfirst_module_entry;
+
 extern zend_module_entry myfirst_moudle_entry;
 
-#endif /* PHP_MYFIRST_H */
+#endif /* MYFIRST_H */
